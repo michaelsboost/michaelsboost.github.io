55d3d0239cc2d590acfbce70d53cd727bcb57a74
Michael <MIchael@MichaelsMacBook.attlocal.net>
2017-07-10 21:44:27 -0500

    run box layout after load function

--- js/index.js
+++ js/index.js
@@ -1,31 +1,29 @@
-$(function() {
-  Boxlayout.init();
-  
-  // Adjust UI for orientation changes
-  $(window).on("load resize orientationchange", function() {
-    if(window.innerHeight > window.innerWidth){
-      // portrait
-      $(".bl-main > section > .bl-box").css("background-size", "cover");
-    } else {
-      // landscape
-      $(".bl-main > section > .bl-box").css("background-size", "contain");
-    }
-  });
-  
-  // Detect if page has hash when loaded
-  var hash = window.location.hash.substring(1);
-  if (window.location.hash) {
-    $("a[href='#"+hash+"']").trigger("click");
+// Adjust UI for orientation changes
+$(window).on("load resize orientationchange", function() {
+  if(window.innerHeight > window.innerWidth){
+    // portrait
+    $(".bl-main > section > .bl-box").css("background-size", "cover");
+  } else {
+    // landscape
+    $(".bl-main > section > .bl-box").css("background-size", "contain");
   }
-  
-  function remove_hash_from_url() {
-    var uri = window.location.toString();
-    if (uri.indexOf("#") > 0) {
-        var clean_uri = uri.substring(0, uri.indexOf("#"));
-        window.history.replaceState({}, document.title, clean_uri);
-    }
+});
+
+Boxlayout.init();
+
+// Detect if page has hash when loaded
+var hash = window.location.hash.substring(1);
+if (window.location.hash) {
+  $("a[href='#"+hash+"']").trigger("click");
+}
+
+function remove_hash_from_url() {
+  var uri = window.location.toString();
+  if (uri.indexOf("#") > 0) {
+      var clean_uri = uri.substring(0, uri.indexOf("#"));
+      window.history.replaceState({}, document.title, clean_uri);
   }
-  $("[data-clear=hash]").click(function() {
-    remove_hash_from_url();
-  });
+}
+$("[data-clear=hash]").click(function() {
+  remove_hash_from_url();
 });
